623dee7b3c5cafe5622d8c96b1e1b6f20bfb3e98
AdarshDalai <adarshkumar.dalai@gmail.com>
2025-06-13 21:28:11 +0530

    updated config for custom dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cloudsbay.github.io',
+    site: 'https://cloudsbay.com',
 
 });
